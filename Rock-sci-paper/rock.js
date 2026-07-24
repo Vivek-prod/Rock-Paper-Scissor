c6c6cee456e0d09a596c394c7fa766e9c7e5d7aa
Vivek <krishnacreations745@gmail.com>
2026-07-25 02:04:25 +0530

    created the main game of rock paper scissor function

--- Rock-sci-paper/rock.js
+++ Rock-sci-paper/rock.js
@@ -9,7 +9,7 @@ function comp() {
 
   if (pc === 0) {
     return "rock";
-  } else if (pc == 1) {
+  } else if (pc === 1) {
     return "scissor";
   } else {
     return "paper";
@@ -19,16 +19,51 @@ function comp() {
 function main() {
   let a = 0;
   let b = 0;
-  let c = 1;
   console.log("WELCOME TO GAME");
-  console.log("Round:", c);
   console.log("YOU:0  PC:0");
 
-  let userchoice = user();
-  console.log(userchoice);
+  game(a, b);
+}
 
-  let pcchoice = comp();
-  console.log(pcchoice);
+function game(a, b) {
+  for (let c = 1; c <= 5; c++) {
+    let userchoice = user();
+    console.log("USER:", userchoice);
+
+    let pcchoice = comp();
+    console.log("COMPUTER:", pcchoice);
+
+    if (userchoice === "rock" && pcchoice === "paper") {
+      b = b + 1;
+      console.log("Round", c, "won by:COMPUTER");
+    } else if (userchoice === "rock" && pcchoice === "scissor") {
+      a = a + 1;
+      console.log("Round", c, "won by:USER");
+    } else if (userchoice === "paper" && pcchoice === "scissor") {
+      b = b + 1;
+      console.log("Round", c, "won by:COMPUTER");
+    } else if (userchoice === "paper" && pcchoice === "rock") {
+      a = a + 1;
+      console.log("Round", c, "won by:USER");
+    } else if (userchoice === "scissor" && pcchoice === "rock") {
+      b = b + 1;
+      console.log("Round", c, "won by:COMPUTER");
+    } else if (userchoice === "scissor" && pcchoice === "paper") {
+      a = a + 1;
+      console.log("Round", c, "won by:COMPUTER");
+    } else {
+      console.log("Round", c, ": Tie");
+    }
+  }
+  console.log("USER won:", a);
+
+  console.log("COMPUTER won:", b);
+
+  if (a > b) {
+    console.log("USER WON OVERALL");
+  } else {
+    console.log("COMPUTER WON OVERALL");
+  }
 }
 
 main();
